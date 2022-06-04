c59412c6d227740eca7cd8f179d88e70d35556b0
robotpsychology <42260976+robotpsychology@users.noreply.github.com>
2022-06-04 15:04:27 -0400

    Got images to display in resources tab.

--- client/src/views/Resources.vue
+++ client/src/views/Resources.vue
@@ -3,7 +3,21 @@
     <div>Resources</div>
     <div v-for="file in files">
       <div>file id: {{ file.id }}</div>
-      <img :src="'localhost:8055/assets/' + file.id" alt="hi" />
+      <img
+        :src="
+          '//localhost:8055/assets/' +
+          file.filename_disk +
+          '?fit=outside&width=100&height=100&quality=80'
+        "
+        alt=""
+        async="on"
+      />
+      <!-- <div
+        class="image-div"
+        :style="{
+          backgroundImage: `url(localhost:8055/assets/${file.filename_disk}/?fit=cover&width=200&height=200&quality=80)`,
+        }"
+      ></div> -->
     </div>
   </div>
 </template>
@@ -27,7 +41,9 @@ export default {
 
 <style>
 img {
-  width: 100%;
-  height: 100%;
+}
+
+.image-div {
+  background-image: url();
 }
 </style>
